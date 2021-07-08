e1a469af270ac201c65fd5f4bca96e16cae8c6cf
EdwinRomeroU <jedcaf@gmail.com>
2021-07-07 20:35:52 -0500

    Update AlbumShareModel.

--- src/main/java/com/wchallenge/models/AlbumShareModel.java
+++ src/main/java/com/wchallenge/models/AlbumShareModel.java
@@ -1,12 +1,12 @@
 package com.wchallenge.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//cart
 @Entity
 @Table(name = "Album_User")
 public class AlbumShareModel {
@@ -16,12 +16,19 @@ public class AlbumShareModel {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable=false)
 	private Integer id;
 	
 	private Integer idUser;
 	private Integer idAlbum;
 	private String userGrant;
 	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public Integer getIdUser() {
 		return idUser;
 	}
